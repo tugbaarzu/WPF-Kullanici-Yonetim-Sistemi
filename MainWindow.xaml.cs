2205dcb138159cd7e25b8184a25e344af8442ca6
Tuğba Arzu Ünal <33021903+tugbaarzu@users.noreply.github.com>
2025-11-15 13:22:42 +0300

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication2
+{
+   
+    public partial class MainWindow : Window
+    {
+        ServiceReference1.Service1Client baglanti = new ServiceReference1.Service1Client();
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+         
+           
+        }
+
+       
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            ServiceReference1.Kullanici a = new ServiceReference1.Kullanici();
+            a.Kullad = kulad.Text;
+            a.Sifre=sifre.Text;
+
+            a.Isim="*";
+            a.Soyisim="*";
+            a.Email="*";
+
+            if (baglanti.YeniKullanici(a) == true)
+            {
+                MessageBox.Show("Eklendi");
+            }
+            else
+            {
+                MessageBox.Show("Eklenmedi");
+            }
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            if (baglanti.SifreKontrol(kulad.Text, sifre.Text) == true)
+            {
+                MessageBox.Show("Giris Yapıldı");
+                Window1 wn1 = new Window1();
+                wn1.ShowDialog();
+            }
+            else
+            {
+                MessageBox.Show("Seni Tanımıyorum....");
+            }
+        }
+
+       
+    }
+}
---
+++ Window1.xaml.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApplication2
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        class nesne
+        {
+            public double en = 50;
+            public double boy = 50;
+
+            public int fx = 0;
+            public int fy = 0;
+
+            public double konumx { get; set; }
+            public double konumy { get; set; }
+
+           
+
+        }
+        nesne eleman = new nesne();
+        public Window1()
+        {
+            InitializeComponent();
+            Ekran.DataContext = eleman;
+            eleman.konumx = 0;
+            eleman.konumy = 0;
+        }
+        //private void rec1_KeyDown(object sender, KeyEventArgs e)
+        //{
+        //    int sol;
+        //    int ust;
+        //    ust = Convert.ToInt32(Canvas.GetTop(rec1));
+        //    sol = Convert.ToInt32(Canvas.GetLeft(rec1));
+
+        //    if (e.Key == Key.Left)
+        //    {
+        //        Canvas.SetLeft(rec1, sol - 20);
+             
+        //    }
+        //    else if (e.Key == Key.Right)
+        //    {
+        //        Canvas.SetLeft(rec1, sol -20);
+        //    }
+        //    else if (e.Key == Key.Down)
+        //    {
+        //        Canvas.SetTop(rec1, ust + -20);
+        //    }
+        //    else if (e.Key == Key.Up)
+        //    {
+        //        Canvas.SetTop(rec1, ust - 20);
+        //    }
+
+        //}
+        private void rec1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Up)
+            {
+                eleman.fy = 0;
+                eleman.fx++;
+                if (eleman.fx > 4) eleman.fx = 0;
+                eleman.konumy -= 5;
+                if (eleman.konumy < 0)
+                {
+                    eleman.konumy = 0;
+                }
+
+            }
+            if (e.Key == Key.Left)
+            {
+                eleman.fy = 0;
+                eleman.fx++;
+                if (eleman.fx > 4) eleman.fx = 0;
+                eleman.konumx -= 5;
+
+                if (eleman.konumx < 0)
+                {
+                    eleman.konumx = 0;
+                }
+            }
+            if (e.Key == Key.Right)
+            {
+                eleman.fy = 0;
+                eleman.fx++;
+                if (eleman.fx > 4) eleman.fx = 0;
+                eleman.konumx += 5;
+
+                if (eleman.konumx > 230)
+                {
+                    eleman.konumx = 230;
+                }
+            }
+            if (e.Key == Key.Down)
+            {
+                eleman.fy = 0;
+                eleman.fx++;
+                if (eleman.fx > 4) eleman.fx = 0;
+                eleman.konumy += 5;
+
+                if (eleman.konumy > 210)
+                {
+                    eleman.konumy = 210;
+                }
+            }
+
+            Ekran.DataContext = "";
+            Ekran.DataContext = eleman;
+        }
+    }
+}
